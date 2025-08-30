94c926e603a0913aa69a7315cdcbf286cd02186c
Javad-code-stack <javadghafouri1999@gmail.com>
2025-08-30 22:07:02 +0330

    deploy: configure production build and environment variables
    - Update backend server to serve static files from frontend build
    - Modify axios and socket.io URLs to use environment-specific values
    - Set up package.json scripts for production build and start
    - Implement environment variable-based port configuration in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 /* --------------------------------- Imports -------------------------------- */
 // Lib
 import "dotenv/config";
-import express from "express";
 import cors from "cors";
+import path from "path";
+import express from "express";
 import cookieParser from "cookie-parser";
 // Functions
 import dbConnect from "./lib/database.js";
@@ -12,7 +13,9 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 /* ------------------------------- App Configs ------------------------------ */
-const port = process.env.PORT || 5001;
+const port = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -28,7 +31,16 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+/* ---------------------------------- Build --------------------------------- */
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+	});
+}
+
 /* -------------------------------- Listening ------------------------------- */
+
 server.listen(port, () => {
 	dbConnect();
 	console.log(`Running on ${port}, Click on http://localhost:8001`);
